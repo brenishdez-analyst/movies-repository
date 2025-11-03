3a548cd83112e81795caffb391d81f5c2a8938c6
brenishdez-analyst <brenishdez@gmail.com>
2025-11-03 16:21:34 -0600

    Update text in app.py
    Update headers, add table viewer.

--- app.py
+++ app.py
@@ -3,14 +3,21 @@ import plotly.express as px
 import streamlit as st
      
 movie_data = pd.read_csv('movies.csv') # leer los datos
-st.header("Popularidad segun genero de peliculas")
+st.header("Proyecto Sprint 7 :)")
+
+st.header("Data Viewer")
+
+data = pd.DataFrame(movie_data, columns=['genres','original_title' ,'popularity', 'revenue'])
+st.write(data)
+
+st.header("Popularidad según género de películas:")
 # hist_button = st.button('Construir histograma') # crear un botón
-build_histogram = st.checkbox('Construir histograma')
+build_histogram = st.checkbox('Da click en la casilla para generar el histograma')
 
      
 if build_histogram: # al hacer clic en el botón
      # escribir un mensaje
-     st.write('Creación de un histograma para el conjunto de datos peliculas')
+     st.write('Histograma de popularidad según el género de películas')
      
      # crear un histograma
      fig = px.histogram(movie_data, x="genres", y="popularity")
@@ -18,12 +25,12 @@ if build_histogram: # al hacer clic en el botón
      # mostrar un gráfico Plotly interactivo
      st.plotly_chart(fig, use_container_width=True)
 
-st.header("Ingreso por genero")
-disp_button = st.button('Construir grafico de dispersion') # crear un botón
+st.header("Ganancias por género:")
+disp_button = st.button('Da click aquí') # crear un botón
 
 if disp_button: # al hacer clic en el botón
      # escribir un mensaje
-     st.write('Creación de un grafico de dispersion para el conjunto de datos peliculas')
+     st.write('Gráfico de dispersión para determinar las ganancias obtenidas por género')
      
      # crear un grafico de dispersion
      fig = px.scatter(movie_data, x="genres", y="revenue")
